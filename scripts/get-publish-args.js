f2eb3d324ca763a1ad3770371d245031d5b55cba
teddyviking <rtejado90@gmail.com>
2018-02-15 17:31:42 +0100

    fix spawn args

--- scripts/get-publish-args.js
+++ scripts/get-publish-args.js
@@ -3,7 +3,8 @@ const config = require('./config');
 module.exports = function getPublishArgs() {
   const args = ['publish'];
   if (config.deployEnv !== 'production') {
-    args.push(`--release-channel ${config.deployEnv}`);
+    args.push('--release-channel');
+    args.push(`${config.deployEnv}`);
   }
   return args;
 };
